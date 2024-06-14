1fbb64f0b53b6e2a275fd9b5e0e46792654f09cf
sooshiance <s.katafract@gmail.com>
2024-06-14 14:56:03 +0330

    register store models to admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
 
-# Register your models here.
+from store.models import (
+    Category, Product, Gallery, Feature
+)
+
+
+######################################## Product registry ########################################
+
+
+class GalleryInline(admin.TabularInline):
+    model = Gallery
+
+
+class FeatureInline(admin.TabularInline):
+    model = Feature
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'active')
+    list_filter = ['active']
+    search_fields = ['title']
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ['active']
+    search_fields = ['title']
+    inlines = [GalleryInline, FeatureInline]
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+######################################## Cart registry ########################################
